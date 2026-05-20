f27bda61224d03fb8ab27d67f180bbc5cd21f6e2
Anfas <anfas010101@gmail.com>
2026-05-20 18:54:10 +0530

    check cart empty or not

--- script.js
+++ script.js
@@ -81,32 +81,38 @@ function generateHtml() {
 
 function carts() {
   let html = "";
-  cart.forEach((item) => {
-    const product = database.find((product) => product.id === item.productId);
-    if (product) {
-      html += `
-            <div class="cart-container">
-                <img src="${product.image}">
-                <div class="cart-info-action-container">
-                    <div class="cart-info">
-                    <p class="name">${product.name}</p>
-                    <span>$${product.price}</span>
-                    </div>
-                    <div class="cart-action">
-                    <span class="cart-quantity">
-                        <i class="fa-solid fa-minus quantity-minus-btn" data-id="${product.id}"></i>
-                        <span class="quantity">${item.quantity}</span>
-                        <i class="fa-solid fa-plus quantity-plus-btn" data-id="${product.id}"></i>
-                    </span>
-                    <span class="cart-remove" data-id="${product.id}">Remove</span>
-                    </div>
-                </div>
-            </div>
-        `;
-    }
-  });
-  cartsGridEl.innerHTML = html;
 
+  if (cart.length > 0) {
+    cart.forEach((item) => {
+      const product = database.find((product) => product.id === item.productId);
+      if (product) {
+        html += `
+              <div class="cart-container">
+                  <img src="${product.image}">
+                  <div class="cart-info-action-container">
+                      <div class="cart-info">
+                      <p class="name">${product.name}</p>
+                      <span>$${product.price.toFixed(2)}</span>
+                      </div>
+                      <div class="cart-action">
+                      <span class="cart-quantity">
+                          <i class="fa-solid fa-minus quantity-minus-btn" data-id="${product.id}"></i>
+                          <span class="quantity">${item.quantity}</span>
+                          <i class="fa-solid fa-plus quantity-plus-btn" data-id="${product.id}"></i>
+                      </span>
+                      <span class="cart-remove" data-id="${product.id}">Remove</span>
+                      </div>
+                  </div>
+              </div>
+          `;
+      }
+    });
+  } else {
+    html =
+      '<p style="text-align: center; padding: 20px;">Your cart is empty.</p>';
+  }
+
+  cartsGridEl.innerHTML = html;
   attachCartListeners();
 }
 
