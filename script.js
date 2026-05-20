6bb6c28f0229e2023d0a15970153d5bc5684e60d
Anfas <anfas010101@gmail.com>
2026-05-20 15:25:48 +0530

    made cart quantity button interactive

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const gridContainer = document.querySelector(".grid-container");
 const cartBtn = document.querySelector(".cart-btn");
 const cartCloseBtn = document.querySelector(".cart-close-btn");
 
-
-
-
 cartBtn.addEventListener("click", () => {
   const carts = document.querySelector(".carts");
   carts.style.transform = "translateX(0)";
@@ -43,6 +40,7 @@ function addToCart() {
         const cartItem = {
           id: cart.length ? cart[cart.length - 1].id + 1 : 1,
           productId: id,
+          quantity: 1,
         };
         cart.push(cartItem);
       } else {
@@ -50,7 +48,22 @@ function addToCart() {
       }
       localStorage.setItem("cart", JSON.stringify(cart));
       carts();
+    });
+  });
 
+  const cartItemQuantity = document.querySelectorAll(".quantity");
+  const quantityMinusBtn = document.querySelectorAll(".quantity-minus-btn");
+  const quantityPlusBtn = document.querySelectorAll(".quantity-plus-btn");
+
+  quantityPlusBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      cartQuantity(btn, "plus");
+    });
+  });
+
+  quantityMinusBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      cartQuantity(btn, "minus");
     });
   });
 }
@@ -84,7 +97,7 @@ function carts() {
   cart.forEach((item) => {
     const product = database.find((product) => product.id === item.productId);
     if (product) {
-    html += `
+      html += `
             <div class="cart-container">
                 <img src="${product.image}">
                 <div class="cart-info-action-container">
@@ -94,9 +107,9 @@ function carts() {
                     </div>
                     <div class="cart-action">
                     <span class="cart-quantity">
-                        <i class="fa-solid fa-minus"></i>
-                        <span>1</span>
-                        <i class="fa-solid fa-plus"></i>
+                        <i class="fa-solid fa-minus quantity-minus-btn" data-id="${product.id}"></i>
+                        <span class="quantity">${item.quantity}</span>
+                        <i class="fa-solid fa-plus quantity-plus-btn" data-id="${product.id}"></i>
                     </span>
                     <span class="cart-remove" data-id="${product.id}">Remove</span>
                     </div>
@@ -107,15 +120,34 @@ function carts() {
   });
   cartsGridEl.innerHTML = html;
 
-    const cartRemoveEl = document.querySelectorAll(".cart-remove");
+  const cartRemoveEl = document.querySelectorAll(".cart-remove");
 
-    cartRemoveEl.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            console.log('delt')
-            const id = Number(btn.dataset.id);
-            cart = cart.filter((item) => item.productId !== id);
-            localStorage.setItem("cart", JSON.stringify(cart));
-            render();
-        });
-    });
+  cartRemoveEl.forEach((btn) => {
+    cartRemove(btn);
+  });
+}
+
+function cartRemove(btn) {
+  btn.addEventListener("click", () => {
+    const id = Number(btn.dataset.id);
+    cart = cart.filter((item) => item.productId !== id);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    render();
+  });
+}
+
+function cartQuantity(btn, operation) {
+  const id = Number(btn.dataset.id);
+  cart = cart.map((cartItem) => {
+    if (cartItem.productId === id) {
+      if (operation === "plus") {
+        cartItem.quantity++;
+      } else if (operation === "minus" && cartItem.quantity > 1) {
+        cartItem.quantity--;
+      }
+    }
+    return cartItem;
+  });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  render();
 }
