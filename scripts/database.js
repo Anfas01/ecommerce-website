0af778f383fee46e936b942250e434430d14dff7
Anfas <anfas010101@gmail.com>
2026-06-08 12:30:49 +0530

    change image url to lower case

--- scripts/database.js
+++ scripts/database.js
@@ -8,279 +8,279 @@ export const database = [
     name: "Men's Figure Graphic Round Neck Tee",
     price: 24.00,
     type: "top",
-    image: "Images/T-Shirt_1_-removebg-preview.webp"
+    image: "images/T-Shirt_1_-removebg-preview.webp"
   },
   {
     id: 2,
     name: "Abstract Mask Print Oversized T-Shirt",
     price: 23.99,
     type: "top",
-    image: "Images/T-Shirt_2_-removebg-preview.webp"
+    image: "images/T-Shirt_2_-removebg-preview.webp"
   },
   {
     id: 3,
     name: "Slim Fit Crew Neck T-Shirt",
     price: 8.99,
     type: "top",
-    image: "Images/T-Shirt_3_-removebg-preview.webp"
+    image: "images/T-Shirt_3_-removebg-preview.webp"
   },
   {
     id: 4,
     name: "American Eagle Super Soft Long Sleeve Rugby Shirt",
     price: 49.50,
     type: "top",
-    image: "Images/T-Shirt_4_-removebg-preview.webp"
+    image: "images/T-Shirt_4_-removebg-preview.webp"
   },
   {
     id: 5,
     name: "Old Navy Soft-Washed Long-Sleeve Raglan Tee",
     price: 16.99,
     type: "top",
-    image: "Images/T-Shirt_5_-removebg-preview.webp"
+    image: "images/T-Shirt_5_-removebg-preview.webp"
   },
   {
     id: 6,
     name: "Men's H202 Runner Patchwork Sweatshirt",
     price: 15.00,
     type: "top",
-    image: "Images/T-Shirt_6_-removebg-preview.webp"
+    image: "images/T-Shirt_6_-removebg-preview.webp"
   },
   {
     id: 7,
     name: "Women's Oversized Striped Poplin Shirt",
     price: 39.90,
     type: "top",
-    image: "Images/Shirt_1_-removebg-preview.webp"
+    image: "images/Shirt_1_-removebg-preview.webp"
   },
   {
     id: 8,
     name: "Striped Shirt & T-Shirt Pack",
     price: 23.40,
     type: "top",
-    image: "Images/Shirt_2_-removebg-preview.webp"
+    image: "images/Shirt_2_-removebg-preview.webp"
   },
   {
     id: 9,
     name: "Women Crinkled Burgundy Shirt",
     price: 21.50,
     type: "top",
-    image: "Images/Shirt_3_-removebg-preview.webp"
+    image: "images/Shirt_3_-removebg-preview.webp"
   },
   {
     id: 10,
     name: "Women Cotton Linen-Blend Shirt",
     price: 29.99,
     type: "top",
-    image: "Images/Shirt_4_-removebg-preview.webp"
+    image: "images/Shirt_4_-removebg-preview.webp"
   },
   {
     id: 11,
     name: "Men's Black Edition Crop Textured Shirt",
     price: 45.90,
     type: "top",
-    image: "Images/Shirt_5_-removebg-preview.webp"
+    image: "images/Shirt_5_-removebg-preview.webp"
   },
   {
     id: 12,
     name: "Burgundy Oversized Corduroy Shirt",
     price: 19.49,
     type: "top",
-    image: "Images/Shirt_6_-removebg-preview.webp"
+    image: "images/Shirt_6_-removebg-preview.webp"
   },
   {
     id: 13,
     name: "Men's Vertical Striped Button Up Shirt",
     price: 14.49,
     type: "top",
-    image: "Images/Shirt_7_-removebg-preview.webp"
+    image: "images/Shirt_7_-removebg-preview.webp"
   },
   {
     id: 14,
     name: "Women Wide Pull-On Pinstripe Trousers",
     price: 34.99,
     type: "bottoms",
-    image: "Images/Pant_1_-removebg-preview.webp"
+    image: "images/Pant_1_-removebg-preview.webp"
   },
   {
     id: 15,
     name: "Women Wide Tailored Dark Brown Trousers",
     price: 39.99,
     type: "bottoms",
-    image: "Images/Pant_2_-removebg-preview.webp"
+    image: "images/Pant_2_-removebg-preview.webp"
   },
   {
     id: 16,
     name: "Women Wide High Jeans Light Denim Blue",
     price: 39.99,
     type: "bottoms",
-    image: "Images/Pant_3_-removebg-preview.webp"
+    image: "images/Pant_3_-removebg-preview.webp"
   },
   {
     id: 17,
     name: "Baggy Balloon Jeans Black",
     price: 49.90,
     type: "bottoms",
-    image: "Images/Pant_4_-removebg-preview.webp"
+    image: "images/Pant_4_-removebg-preview.webp"
   },
   {
     id: 18,
     name: "Women Plush Wide-Leg Black Jogging Pants",
     price: 35.90,
     type: "bottoms",
-    image: "Images/Pant_5_-removebg-preview.webp"
+    image: "images/Pant_5_-removebg-preview.webp"
   },
   {
     id: 19,
     name: "Women Wide-Leg Light Gray Melange Joggers",
     price: 24.99,
     type: "bottoms",
-    image: "Images/Pant_6_-removebg-preview.webp"
+    image: "images/Pant_6_-removebg-preview.webp"
   },
   {
     id: 20,
     name: "Adidas Originals Firebird Track Pants",
     price: 80.00,
     type: "bottoms",
-    image: "Images/Pant_7_-removebg-preview.webp"
+    image: "images/Pant_7_-removebg-preview.webp"
   },
   {
     id: 21,
     name: "Cider High Waist Solid Pleated Wide Leg Trousers",
     price: 28.00,
     type: "bottoms",
-    image: "Images/Pant_8_-removebg-preview.webp"
+    image: "images/Pant_8_-removebg-preview.webp"
   },
   {
     id: 22,
     name: "Regular Fit Black Crewneck Sweatshirt",
     price: 19.99,
     type: "top",
-    image: "Images/Hoodie_1_-removebg-preview.webp"
+    image: "images/Hoodie_1_-removebg-preview.webp"
   },
   {
     id: 23,
     name: "Frances Austen Frances Crewneck Cashmere Sweater Burgundy",
     price: 39.99,
     type: "top",
-    image: "Images/Hoodie_2_-removebg-preview.webp"
+    image: "images/Hoodie_2_-removebg-preview.webp"
   },
   {
     id: 24,
     name: "Temu Men's Gothic Dark Style Multi-Position Print Zip-Up Hoodie",
     price: 23.27,
     type: "top",
-    image: "Images/Hoodie_3_-removebg-preview.webp"
+    image: "images/Hoodie_3_-removebg-preview.webp"
   },
   {
     id: 25,
     name: "Manfinity Men New York Yankees Print Crewneck Sweatshirt White",
     price: 14.00,
     type: "top",
-    image: "Images/Hoodie_4_-removebg-preview.webp"
+    image: "images/Hoodie_4_-removebg-preview.webp"
   },
   {
     id: 26,
     name: "Fanatics Men's Las Vegas Raiders NFL Core Foundation Jersey",
     price: 39.99,
     type: "top",
-    image: "Images/Jersey_1_-removebg-preview.webp"
+    image: "images/Jersey_1_-removebg-preview.webp"
   },
   {
     id: 27,
     name: "Real Madrid Emirates Fly Better Crewneck Sweatshirt",
     price: 35.00,
     type: "top",
-    image: "Images/Jersey_2_-removebg-preview.webp"
+    image: "images/Jersey_2_-removebg-preview.webp"
   },
   {
     id: 28,
     name: "Men Zip Up PU Leather Jacket",
     price: 47.89,
     type: "top",
-    image: "Images/Jacket_1_-removebg-preview.webp"
+    image: "images/Jacket_1_-removebg-preview.webp"
   },
   {
     id: 29,
     name: "Zara Faux Leather Jacket Burgundy",
     price: 89.90,
     type: "top",
-    image: "Images/Jacket_2_-removebg-preview.webp"
+    image: "images/Jacket_2_-removebg-preview.webp"
   },
   {
     id: 30,
     name: "Men's Washed Ruthlessness Graphic Baseball Cap",
     price: 6.25,
     type: "accessories",
-    image: "Images/Cap_1_-removebg-preview.webp"
+    image: "images/Cap_1_-removebg-preview.webp"
   },
   {
     id: 31,
     name: "Men Trucker Hat Geometric Embroidered Baseball Cap",
     price: 5.99,
     type: "accessories",
-    image: "Images/Cap_2_-removebg-preview.webp"
+    image: "images/Cap_2_-removebg-preview.webp"
   },
   {
     id: 32,
     name: "Lefties Embroidered Striped Check Shirt",
     price: 22.50,
     type: "top",
-    image: "Images/Shirt_8_-removebg-preview.webp"
+    image: "images/Shirt_8_-removebg-preview.webp"
   },
   {
     id: 33,
     name: "NeonVogueWave Men's Vintage Denim Shirt",
     price: 32.99,
     type: "top",
-    image: "Images/Shirt_9_-removebg-preview.webp"
+    image: "images/Shirt_9_-removebg-preview.webp"
   },
   {
     id: 34,
     name: "Diamond Layla Crease Finish Vintage-Red Check Shirt",
     price: 59.00,
     type: "top",
-    image: "Images/Shirt_10_-removebg-preview.webp"
+    image: "images/Shirt_10_-removebg-preview.webp"
   },
   {
     id: 35,
     name: "Regular Fit Oxford Shirt",
     price: 18.00,
     type: "top",
-    image: "Images/Shirt_11_-removebg-preview.webp"
+    image: "images/Shirt_11_-removebg-preview.webp"
   },
   {
     id: 36,
     name: "LCW Vision Regular Fit Resort Collar Men's Shirt",
     price: 16.50,
     type: "top",
-    image: "Images/Shirt_12_-removebg-preview.webp"
+    image: "images/Shirt_12_-removebg-preview.webp"
   },
   {
     id: 37,
     name: "COOFANDY Men's Corduroy Long Sleeve Button Down Shirt",
     price: 29.99,
     type: "top",
-    image: "Images/Shirt_13_-removebg-preview.webp"
+    image: "images/Shirt_13_-removebg-preview.webp"
   },
   {
     id: 38,
     name: "ZAFUL Men's Corduroy Pocket Long Sleeve Shirt",
     price: 25.99,
     type: "top",
-    image: "Images/Shirt_14_-removebg-preview.webp"
+    image: "images/Shirt_14_-removebg-preview.webp"
   },
   {
     id: 39,
     name: "Reiss Cedric Textured Cuban-Collar Shirt",
     price: 150.00,
     type: "top",
-    image: "Images/Shirt_15_-removebg-preview.webp"
+    image: "images/Shirt_15_-removebg-preview.webp"
   },
   {
     id: 40,
     name: "COOFANDY Men's Casual Long Sleeve Cotton Linen Shirt",
     price: 25.99,
     type: "top",
-    image: "Images/Shirt_16_-removebg-preview.webp"
+    image: "images/Shirt_16_-removebg-preview.webp"
   }
 ];
